ac314c65dea1df4eb938614b778379e858763a7f
itsar-t <rasti@student.chalmers.se>
2016-01-07 07:12:44 +0100

    moreInTesting
    trying to make more tests

--- Hotel/src/test/TestStart.java
+++ Hotel/src/test/TestStart.java
@@ -25,10 +25,14 @@ public class TestStart {
 		hc.getGuestInterface().createGuest("234567", "Rasti", "Tengman", "rasti@lalala.com", "0736332151");
 		hc.getGuestInterface().createGuest("345678", "Tom", "Foughman", "tobbe@lalala.com", "0707111111");
 		
+		/*
+		 *  Test what happens if we try to book a room that is not available 
+		 */
 		System.out.println(hc.getBookingInterface().availableTypes(new Date(2016,01,07),new Date(2016,01,14)));
-		hc.getBookingInterface().createBooking("123456", new Date(2016,01,07), new Date(2016,01,14), "single");
-		hc.getBookingInterface().createBooking("234567", new Date(2016,01,07), new Date(2016,01,14), "single");
-		hc.getBookingInterface().getBookings("123456");
-		System.out.println(hc.getBookingInterface().getBookings("123456"));
+		hc.getBookingInterface().createBooking("123456", new Date(2016,01,07), new Date(2016,01,14), "double");
+		hc.getBookingInterface().createBooking("234567", new Date(2016,01,07), new Date(2016,01,14), "double");
+		//hc.getBookingInterface().displayBookingInfo(hc.getBookingInterface().getBookings("123456").indexOf(0));
+		//System.out.println(hc.getBookingInterface().displayBookingInfo(hc.getBookingInterface().getBookings("123456").indexOf(0)));
+		hc.getBookingInterface().createBooking("345678",new Date(2016,01,07), new Date(2016,01,14), "double");
 	}
 }
