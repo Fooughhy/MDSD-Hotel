c7f46ebeed33355a02a52cf526d5488badfa4c4d
Alexander Hederstaf <alexander.hederstaf@gmail.com>
2016-01-07 07:32:51 +0100

    Unused imports

--- Hotel/src/test/Testing.java
+++ Hotel/src/test/Testing.java
@@ -1,12 +1,6 @@
 package test;
 
-import component.model.Hotel;
-import component.model.Room;
-import component.model.RoomType;
-
 public class Testing {
-	
-
 	
 	public static void main(String[] args){
 		new TestStart().start();
